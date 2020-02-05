507d0c0939d772c0c53a27031b6406baac81020c
Elliot Jordan <elliot.jordan@and.digital>
2020-02-05 11:55:12 +0000

    cerated snapshot test

---
+++ src/components/ProjectModal/ProjectModal.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ProjectModal from './ProjectModal';
+
+describe('Header component', () => {
+  it('renders correctly', () => {
+    const wrapper = render(<ProjectModal image="http://2.bp.blogspot.com/_xmTzO3Zx0ZQ/TJCcIqVZGoI/AAAAAAAAE1M/fR7XKlnGR20/s1600/robot-posters-inspiration-01.jpg" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
