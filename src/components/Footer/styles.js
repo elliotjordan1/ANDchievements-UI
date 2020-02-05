45884941a8560cde0d2582db62d177a88ee7308f
Elliot Jordan <elliot.jordan@and.digital>
2020-02-05 17:08:36 +0000

    removed erroneous z-index from footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -8,7 +8,6 @@ export const FooterWrapper = styled.div`
   background-color: ${theme.colours.brand.red};
   position: relative;
   bottom: 0;
-  z-index: 999;
   width: 100%;
   height: 30px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
