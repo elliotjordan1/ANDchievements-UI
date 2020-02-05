ca04b607ba6cf28f2dd83460ebf7a7b120ea0c40
Jaimin Chouhan <jaiminchouhan@Jaimins-MacBook-Pro.local>
2020-02-05 16:49:12 +0000

    snapshot

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Link from 'next/link';
 import { FooterWrapper, FooterText, FooterDiv } from './styles';
-import Logo from '../Logo';
 
 const Header = () => {
   return (
