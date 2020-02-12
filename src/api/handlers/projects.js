0bba26056fa550cf3bfb0a01ae65c3002bb9c0bd
Elliot Jordan <elliot.jordan@and.digital>
2020-02-12 15:51:00 +0000

    prod api

--- src/api/handlers/projects.js
+++ src/api/handlers/projects.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const getAllProjects = async () => {
-  const response = await Axios.get('https://eaiec6cwd2.execute-api.us-east-1.amazonaws.com/dev/projects/get');
+  const response = await Axios.get('https://658m0jk5l3.execute-api.us-east-1.amazonaws.com/prod/projects/get');
   return response;
 };
 
