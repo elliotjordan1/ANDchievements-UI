4d352c20c1a600210fb2e1cb5883be30a7431dc5
Henry Pye <henrypye@outlook.com>
2020-02-18 10:30:04 +0000

    Replaced hardcoded auth token with environment variable

--- src/api/handlers/projects.js
+++ src/api/handlers/projects.js
@@ -1,7 +1,14 @@
 import Axios from 'axios';
 
+const options = {
+  method: 'GET',
+  headers: {
+    'x-api-key' : process.env.AUTHENTICATION_TOKEN
+  }
+};
+
 const getAllProjects = async () => {
-  const response = await Axios.get('https://658m0jk5l3.execute-api.us-east-1.amazonaws.com/prod/projects/get');
+  const response = await Axios.get('https://658m0jk5l3.execute-api.us-east-1.amazonaws.com/prod/projects/get', options);
   return response;
 };
 
