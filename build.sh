89caa0578710436bc575a08f36b894a5b0688a87
Henry Pye <henrypye@outlook.com>
2020-02-19 10:37:47 +0000

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
+echo "* * * * *\n"
+echo " * Building Docker Image...\n"
 docker build -t andchievements-ui:latest --build-arg AUTH_TOKEN=$AUTHENTICATION_TOKEN --build-arg API_URL=$API_URL .
+echo " * Build complete! \n"
+echo "* * * * *\n"
