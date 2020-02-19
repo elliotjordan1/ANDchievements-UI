0f21e351aa9aaf08fa579f3b5aca37b503165dd4
Henry Pye <henrypye@outlook.com>
2020-02-19 10:55:11 +0000

    Removed newline carraige

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-echo "* * * * *\n"
-echo " * Building Docker Image...\n"
+echo "* * * * *"
+echo " * Building Docker Image..."
 docker build -t andchievements-ui:latest --build-arg AUTH_TOKEN=$AUTHENTICATION_TOKEN --build-arg API_URL=$API_URL .
-echo " * Build complete! \n"
-echo "* * * * *\n"
+echo " * Build complete! "
+echo "* * * * *"
