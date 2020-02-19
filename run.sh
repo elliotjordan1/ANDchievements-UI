170bb66570e8716efca5466d7399e442a0ba1dff
Henry Pye <henrypye@outlook.com>
2020-02-19 10:56:15 +0000

    Added run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+echo "* * * * *"
+echo "Running Docker App..."
+docker run -v ${PWD}:/app -v /app/node_modules -p 3000:3000 --rm andchievements-ui:latest
+echo " * Run Complete..."
+echo "* * * * *"
