35921a9f9515cfbd320db34eb8c3daced7689c37
Elliot Jordan <elliot.jordan@and.digital>
2020-02-24 15:45:15 +0000

    updated test for API handler

--- src/api/handlers/getprojects/getProjects.spec.js
+++ src/api/handlers/getprojects/getProjects.spec.js
@@ -1,10 +1,21 @@
 import getAllProjects from './getProjects';
+import makeGetRequest from '../../requests/getRequest';
 
+jest.mock('../../requests/getRequest');
 
 describe('getProjects', () => {
-  it('calls makeGetRequest with correct endpoint', () => {
-    const response = getAllProjects();
-    return expect(response).toEqual({status: 200, projects: {}});
+  it('calls makeGetRequest with correct endpoint', async () => {
+    const mockResponse = {status: 200, data : {projects:[{title:'Project Test'}]}};
+    makeGetRequest.mockReturnValueOnce(Promise.resolve(mockResponse));
+    const response = await getAllProjects();
+    return expect(response).toEqual({status: mockResponse.status, ...mockResponse.data});
   });
   
+  it('returns error message for a 404', async () => {
+    const mockResponse = {status: 404, statusText : 'project not found'};
+    makeGetRequest.mockReturnValueOnce(Promise.resolve(mockResponse));
+    const response = await getAllProjects();
+    return expect(response).toEqual({status: mockResponse.status, message: mockResponse.statusText});
+  });
+
 });
