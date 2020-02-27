5cea705949aff1e1da0c2655a0bf04454eb21f20
Henry Pye <henrypye@outlook.com>
2020-02-27 10:56:25 +0000

    Added shuffleAndSort back in

--- src/pages/index.spec.js
+++ src/pages/index.spec.js
@@ -1,26 +1,27 @@
 /* eslint-disable no-unused-expressions */
-import { sliceArray } from './index';
-
+import { shuffleAndSliceArray } from './index';
 
 describe('index.js tests', () => {
   it('returns the same array when array length is less than slice length', () => {
     const shortArray = [1, 2, 3];
     const sliceLength = 5;
 
-    const expectedResult = shortArray;
+    const expectedResult = [1, 2, 3];
 
-    const actualResult = sliceArray(shortArray, sliceLength);
+    const actualResult = shuffleAndSliceArray(shortArray, sliceLength);
     
-    expect(actualResult).toEqual(expectedResult);
+    expect(actualResult).not.toEqual(expectedResult);
+    expect(actualResult.length).toEqual(expectedResult.length);
   });
   it('returns a sliced array when array length is greater than slice length', () => {
     const shortArray = [1, 2, 3, 4, 5, 6, 7];
     const sliceLength = 5;
 
-    const expectedResult = [1, 2, 3, 4, 5];
+    const expectedResult = [1, 2, 3, 4, 5, 6, 7];
 
-    const actualResult = sliceArray(shortArray, sliceLength);
+    const actualResult = shuffleAndSliceArray(shortArray, sliceLength);
     
-    expect(actualResult).toEqual(expectedResult);
+    expect(actualResult).not.toEqual(expectedResult);
+    expect(actualResult.length).toEqual(sliceLength);
   });
 });
