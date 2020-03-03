55517da80a1bf60c5b2460503003955cb0363df8
Henry Pye <henrypye@outlook.com>
2020-03-03 11:54:52 +0000

    Added responsiveness for addition of clients, tech stacks and ANDis

--- src/components/AttributeForm/AttributeForm.spec.js
+++ src/components/AttributeForm/AttributeForm.spec.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { ToastProvider } from 'react-toast-notifications';
 import AttributeForm from './AttributeForm';
 import * as AttributeTypes from '../../global/constants';
 
 describe('AttributeForm component', () => {
   it('renders correctly for a client', () => {
-    const wrapper = render(<AttributeForm formType={AttributeTypes.Client} />);
+    const wrapper = render(<ToastProvider><AttributeForm formType={AttributeTypes.Client} /></ToastProvider>);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('renders correctly for a Tech Stack', () => {
-    const wrapper = render(<AttributeForm formType={AttributeTypes.TechStack}/>);
+    const wrapper = render(<ToastProvider><AttributeForm formType={AttributeTypes.TechStack}/></ToastProvider>);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('renders correctly for an ANDi', () => {
-    const wrapper = render(<AttributeForm formType={AttributeTypes.ANDi}/>);
+    const wrapper = render(<ToastProvider><AttributeForm formType={AttributeTypes.ANDi}/></ToastProvider>);
     expect(wrapper).toMatchSnapshot();
   });
 });
