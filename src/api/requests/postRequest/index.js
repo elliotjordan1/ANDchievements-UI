8352c209917c18867497fc895f8625748735c350
Henry Pye <henrypye@outlook.com>
2020-03-11 19:45:12 +0000

    Attempted inconsistency fix

--- src/api/requests/postRequest/index.js
+++ src/api/requests/postRequest/index.js
@@ -1,25 +1,19 @@
 import Axios from 'axios';
 import { API_URL,  AUTHENTICATION_TOKEN } from 'react-native-dotenv';
 
-const createOptions = (endpoint, content) => {
-  const url = `${API_URL}${endpoint}`;
-  const jsonContent = JSON.stringify(content);
-
-  return (
-  { 
-    method: 'POST',
-    headers: {
-      'x-api-key' : AUTHENTICATION_TOKEN,
-      'Content-Type' : 'application/json'
-    },
-    data: jsonContent,
-    url
-  }
-)};
-
 export default async (endpoint, content) => {
+  const options = {
+    method : 'POST',
+    headers: {
+      'x-api-key': AUTHENTICATION_TOKEN,
+      'Content-Type': 'application/json'
+    },
+    data: JSON.stringify(content),
+    url: `${API_URL}${endpoint}`
+  }
+
   try {
-    return await Axios(createOptions(endpoint, content));
+    return await Axios(options);
   } catch (error) {
     return {
       status: 500,
